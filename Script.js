21865bdb6d9cf558366858a1f4131b01e14809cb
delfivazqueze <126835107+delfivazqueze@users.noreply.github.com>
2023-06-15 15:54:37 -0300

    ff

--- Script.js
+++ Script.js
@@ -1,14 +1,17 @@
 function suscribir() {
-    const emailInput = document.getElementById('emailInput');
-    const email = emailInput.value;
-  
-    if (email === '') {
-      alert('Por favor, ingrese su correo electrónico.');
-      return;
-    }
-  
-    // Aquí puedes agregar tu lógica para enviar el correo de confirmación o guardar el correo en tu base de datos
-    // En este ejemplo, simplemente mostraremos un mensaje en la consola
-    console.log(`Gracias por suscribirte, ${email}!`);
-    emailInput.value = ''; // Limpiar el campo de entrada después de suscribirse
+  const emailInput = document.getElementById('emailInput');
+  const email = emailInput.value;
+
+  if (email === '') {
+    alert('Por favor, ingrese su correo electrónico.');
+    return;
   }
+
+  // Guardar el correo electrónico en tu sistema de almacenamiento (ejemplo: en una base de datos)
+  // Aquí puedes agregar tu lógica para guardar el correo electrónico
+  // En este ejemplo, simplemente mostraremos un mensaje en la consola
+  console.log(`Correo electrónico guardado: ${email}`);
+
+  // Limpiar el campo de entrada después de guardar el correo electrónico
+  emailInput.value = '';
+}
